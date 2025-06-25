e17c43ad48f97c93ad0f41cd2e2f122978181144
AngryHtml <123128711+angryhtml@users.noreply.github.com>
2025-06-26 00:45:00 +0200

    refactor: rewrite JavaScript code to TypeScript

--- script.ts
+++ script.ts
@@ -1,18 +1,19 @@
-"use strict";
-const p1Button = document.querySelector('#p1-button');
-const p2Button = document.querySelector('#p2-button');
-const resetButton = document.querySelector('#reset-button');
-const p1Display = document.querySelector('#p1-display');
-const p2Display = document.querySelector('#p2-display');
-const winningScoreSelect = document.querySelector('#play-to');
+const p1Button = document.querySelector('#p1-button') as HTMLButtonElement;
+const p2Button = document.querySelector('#p2-button') as HTMLButtonElement;
+const resetButton = document.querySelector('#reset-button') as HTMLButtonElement;
+const p1Display = document.querySelector('#p1-display') as HTMLParagraphElement;
+const p2Display = document.querySelector('#p2-display') as HTMLParagraphElement;
+const winningScoreSelect = document.querySelector('#play-to') as HTMLSelectElement;
+
 let winningScore = 3;
 let p1Score = 0;
 let p2Score = 0;
 let isGameOver = false;
-p1Button.addEventListener('click', function () {
-    if (!isGameOver) {
+
+p1Button.addEventListener('click', function() {
+    if(!isGameOver) {
         p1Score += 1;
-        if (p1Score === winningScore) {
+        if(p1Score === winningScore) {
             isGameOver = true;
             p1Display.classList.add('winner');
             p2Display.classList.add('loser');
@@ -20,12 +21,14 @@ p1Button.addEventListener('click', function () {
             p2Button.disabled = true;
         }
         p1Display.textContent = p1Score.toString();
+        
     }
-});
-p2Button.addEventListener('click', function () {
-    if (!isGameOver) {
+})
+
+p2Button.addEventListener('click', function() {
+    if(!isGameOver) {
         p2Score += 1;
-        if (p2Score === winningScore) {
+        if(p2Score === winningScore) {
             isGameOver = true;
             p2Display.classList.add('winner');
             p1Display.classList.add('loser');
@@ -33,14 +36,18 @@ p2Button.addEventListener('click', function () {
             p2Button.disabled = true;
         }
         p2Display.textContent = p2Score.toString();
+        
     }
-});
-winningScoreSelect.addEventListener('change', function () {
+})
+
+winningScoreSelect.addEventListener('change', function(this: HTMLSelectElement) {
     winningScore = parseInt(this.value);
     reset();
-});
-resetButton.addEventListener('click', reset);
-function reset() {
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset () {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
